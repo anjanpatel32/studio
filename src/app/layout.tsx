e20a7344936182168ec260e0055ae1aa4c6ef714
attendspark dev <attendsparkdev@gmail.com>
2025-08-13 20:42:17 +0000

    ca-app-pub-3216800313666122~8822678000 adsense setup and setup finalize

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 import { ThemeProvider } from '@/components/shared/theme-provider';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Student Toolkit',
@@ -19,6 +20,12 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet" />
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3216800313666122"
+          crossOrigin="anonymous"
+          strategy="lazyOnload"
+        />
       </head>
       <body className="font-body antialiased">
         <ThemeProvider
