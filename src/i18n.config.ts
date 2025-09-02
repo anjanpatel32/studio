33e30bdbd48caf4d6fcd69d0e6a8233061bffc5f
attendspark dev <attendsparkdev@gmail.com>
2025-09-02 06:33:16 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/i18n.config.ts
+++
@@ -1,8 +0,0 @@
-import { Config } from 'next-i18n-router/dist/types';
-
-const i18nConfig: Config = {
-  locales: ['en', 'es', 'hi'],
-  defaultLocale: 'en'
-};
-
-export const i18n = i18nConfig;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-import { i18n } from './i18n.config'
+import { languages, fallbackLng } from './app/i18n/settings'
 
 import { match as matchLocale } from '@formatjs/intl-localematcher'
 import Negotiator from 'negotiator'
@@ -11,16 +11,16 @@ function getLocale(request: NextRequest): string | undefined {
   request.headers.forEach((value, key) => (negotiatorHeaders[key] = value))
 
   // @ts-ignore locales are readonly
-  const locales: string[] = i18n.locales
-  const languages = new Negotiator({ headers: negotiatorHeaders }).languages()
+  const locales: string[] = languages
+  const languagesFromHeader = new Negotiator({ headers: negotiatorHeaders }).languages()
 
-  const locale = matchLocale(languages, locales, i18n.defaultLocale)
+  const locale = matchLocale(languagesFromHeader, locales, fallbackLng)
   return locale
 }
 
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname
-  const pathnameIsMissingLocale = i18n.locales.every(
+  const pathnameIsMissingLocale = languages.every(
     locale => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
   )
 
