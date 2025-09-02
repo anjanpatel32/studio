31aaeb12f26b7f187b54ffd9e2e0d24ec85a1131
attendspark dev <attendsparkdev@gmail.com>
2025-09-02 09:07:15 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/i18n/client.ts
+++ src/app/i18n/client.ts
@@ -6,7 +6,6 @@ import i18next, { FlatNamespace, KeyPrefix } from 'i18next'
 import { initReactI18next, useTranslation as useTranslationOrg, UseTranslationOptions, FallbackNs } from 'react-i18next'
 import { useCookies } from 'react-cookie'
 import resourcesToBackend from 'i18next-resources-to-backend'
-import LanguageDetector from 'i18next-browser-languagedetector'
 import { getOptions, cookieName } from './settings'
 
 const runsOnServerSide = typeof window === 'undefined'
@@ -14,7 +13,6 @@ const runsOnServerSide = typeof window === 'undefined'
 //
 i18next
   .use(initReactI18next)
-  .use(LanguageDetector)
   .use(resourcesToBackend((language: string, namespace: string) => import(`../../../public/locales/${language}/${namespace}.json`)))
   .init({
     ...getOptions(),
